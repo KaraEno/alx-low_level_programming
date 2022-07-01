f18af54e5f7050bf790403f92467c6c5ba04a243
KaraEno <abasiakaraalexander@gmail.com>
2022-07-01 08:36:56 -0700

    Completed task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+/**
+*main - Entry point
+*Description: 'the program's description'
+*Return: Always 0 (Success)
+ */
 int main(void)
 {
+
 	char line[] = "Programming is like building a multilingual puzzle\n";
 
 	puts(line);
