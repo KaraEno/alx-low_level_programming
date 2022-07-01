2fa68b6b99f489cbd4715f02e895b74b58e04818
KaraEno <abasiakaraalexander@gmail.com>
2022-07-01 14:15:46 -0700

    Completed task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - entry point
+ * Description: 'write function'
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	char write_up[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, write_up, sizeof(quote) - 1);
+	return (1);
+}
