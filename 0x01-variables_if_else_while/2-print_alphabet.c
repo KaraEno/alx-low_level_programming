5b51b3b25f15a8e9334b687bb0d94acfca5f1ff5
KaraEno <abasiakaraalexander@gmail.com>
2022-07-03 12:04:59 -0700

    Completed task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+* main- entry point
+*Description: 'A to Z in lowercase'
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char x;
+	char lwcase;
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		lwcase = tolower(x);
+		putchar("%c\n", lwcase);
+	}
+	return (0);
+}
