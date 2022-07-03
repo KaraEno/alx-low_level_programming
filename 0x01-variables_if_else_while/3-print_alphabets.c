67c3b5b077be803ef50c446e0eaa904339013557
KaraEno <abasiakaraalexander@gmail.com>
2022-07-03 12:53:11 -0700

    Completed task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main- entry point
+* Description: 'a to z and A to Z'
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char x;
+	char y;
+
+	for (x = 'a'; x <= 'z'; x++)
+		putchar(x);
+	for (y = 'A'; y <= 'Z'; y++)
+		putchar(y);
+	putchar('\n');
+}
