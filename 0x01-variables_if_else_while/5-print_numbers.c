b38f56553e171c6d8d142b9bf73a78e752c053d0
KaraEno <abasiakaraalexander@gmail.com>
2022-07-03 14:35:10 -0700

    Completed task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main- entry point
+* Description:'1 to 10'
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
