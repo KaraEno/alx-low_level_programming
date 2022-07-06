8fc0091f45262dd798f5b895cb0244fb3c580d58
KaraEno <abasiakaraalexander@gmail.com>
2022-07-05 17:32:36 -0700

    Completed task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* main - entry point
+* Description: 'print _putchar'
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char word[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(word[i]);
+	_putchar('\n');
+	return  (0);
+}
