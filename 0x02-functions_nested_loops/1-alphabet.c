e281448920c812b0500744a7ae521ac5b1e351b4
KaraEno <abasiakaraalexander@gmail.com>
2022-07-05 17:42:55 -0700

    Completed task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* main- entry point
+* Description: 'write a letter'
+* Return: Always return 0 (Success)
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		_putchar(x);
+	_putchar('\n');
+	return (0);
+}
