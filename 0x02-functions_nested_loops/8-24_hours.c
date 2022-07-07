86758ea933f26db14abc6c55ca6158c20cc49e76
KaraEno <abasiakaraalexander@gmail.com>
2022-07-06 17:08:48 -0700

    Completed task 7

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* jack_bauer - print 24 hours
+* Return: 0
+*/
+void jack_bauer(void)
+	int hours;
+	int mins;
+	int hrem;
+	int mrem;
+
+	for (hours = 0; hour <= 23; hours++)
+	{
+		for (mins = 0; mins <= 59; mins++)
+		{
+			mrem = mins % 10;
+			hrem = hours % 10;
+			_putchar(hours / 10 + '0');
+			_putchar(hrem + '0');
+			_putchar(':');
+			_putchar(mins / 10 + '0');
+			_putchar(mrem = '0');
+			_putchar('\n');
+		}
+		mins = 0;
+	}
