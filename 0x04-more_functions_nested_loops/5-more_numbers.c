4e39557d696dd12ec69dd61069e230cb849cd542
KaraEno <abasiakaraalexander@gmail.com>
2022-07-08 14:20:12 -0700

    Completed task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* more_numbers - print more nums
+* Return: always 0
+*/
+void more_numbers(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 15; b++)
+		{
+			if (b / 10 != 0)
+				_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
