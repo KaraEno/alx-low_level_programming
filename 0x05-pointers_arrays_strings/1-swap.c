906f6b63044c095545216ca6d01c609b479e1198
KaraEno <abasiakaraalexander@gmail.com>
2022-07-12 15:17:13 -0700

    Completed task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* swap_int - Swap int
+* @a: parameter a
+* @b: parameter b
+* Return: 0
+*/
+void swap_int(int *a, int *b)
+{
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
