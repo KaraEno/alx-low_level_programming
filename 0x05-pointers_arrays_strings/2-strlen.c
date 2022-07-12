715705b997eb2078a7b4b17e2e99f43f47131a40
KaraEno <abasiakaraalexander@gmail.com>
2022-07-12 16:06:43 -0700

    Completed task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _strlen - count
+* @s: param
+* Return: 0
+*/
+int _strlen(char *s)
+{
+	int c;
+
+	while (*s != '\0')
+	{
+		c++;
+		*s++;
+	}
+	return (c);
+}
+
