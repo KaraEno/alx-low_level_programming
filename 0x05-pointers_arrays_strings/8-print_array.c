c63e3ae0a64c776bdc95e2629a0981f8466d85e7
KaraEno <abasiakaraalexander@gmail.com>
2022-07-13 15:52:50 -0700

    Pointers arrays strings

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_array - print array
+* @a: parameter 1
+* @n: parameter 2
+* Return: 0
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		printf("%d, ", a[i]);
+	printf('\n');
+}
+
