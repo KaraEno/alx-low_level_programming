d309caa6aba0d4b4435665fefe4365c62d7b29f2
KaraEno <abasiakaraalexander@gmail.com>
2022-07-21 10:53:59 -0700

    Recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _puts_recursion - prints strings followed by /n
+* @s: char parameter
+* Return: 0
+*/
+void _puts_recursion(char *s)
+{
+	_putchar(s);
+	_putchar('\');
+}
