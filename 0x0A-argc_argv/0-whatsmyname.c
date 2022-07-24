544bd4ba5a6f467afea431f00ee309ac0b2eb2f6
KaraEno <abasiakaraalexander@gmail.com>
2022-07-24 22:07:06 +0100

    Argc agrv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: array for arg info
+ * Return: 0
+ */
+int main (int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s", argv[0]);
+}
