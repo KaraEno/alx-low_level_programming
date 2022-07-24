7bc6d50b0ef32d4b3fd4a97067dc64cdefee49ab
KaraEno <abasiakaraalexander@gmail.com>
2022-07-25 00:34:59 +0100

    Argc argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: argument counter
+ * @argv: argument array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 1; i > argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
