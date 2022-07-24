b001a5a8386b087c8b72884348859ad47510c6c4
KaraEno <abasiakaraalexander@gmail.com>
2022-07-24 23:38:53 +0100

    Argc argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - entry point
+ * @agrc: argument count
+ * @agrv: argument array
+ */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%d\n", argc);
