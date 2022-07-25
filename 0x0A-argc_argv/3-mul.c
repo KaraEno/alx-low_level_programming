735c44d087bac62194a919a62d3c1ef0f5ce94c3
KaraEno <abasiakaraalexander@gmail.com>
2022-07-25 12:37:19 -0700

    Argc argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,20 +6,4 @@
  * @argv: argument array
  * Return: 0
  */
-int main(int argc, char *argv[])
-{
-	int a = atoi(argv[1]);
-	int b = atoi(argv[2]);
-	int c = a * b;
 
-	if (argc != 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	else
-	{
-	printf("%d\n", c);
-	return (0);
-	}
-}
