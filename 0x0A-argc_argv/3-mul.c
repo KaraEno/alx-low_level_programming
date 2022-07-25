a5f4a32253132e19fa1168755ec3307da89c3f7b
KaraEno <abasiakaraalexander@gmail.com>
2022-07-25 01:09:14 +0100

    Argc argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,9 +4,10 @@
  * @argc: argument counter
  * @argv: argument array
  */
-int main (int argc, char *argv)
+int main(int argc, char *argv)
 {
 	int a = atoi(argv[1]) * atoi(argv[2]);
+	
 	if (argc == 2)
 		printf("%d\n", a);
 	else
