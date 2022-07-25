472f7fe6d1c0600bb68d129b3afe504b16bb7028
KaraEno <abasiakaraalexander@gmail.com>
2022-07-25 01:06:36 +0100

    Argc argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: argument counter
+ * @argv: argument array
+ */
+int main (int argc, char *argv)
+{
+	int a = atoi(argv[1]) * atoi(argv[2]);
+	if (argc == 2)
+		printf("%d\n", a);
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
