60710c87e252ffa6b2100332a4ccd825620c7f0f
KaraEno <abasiakaraalexander@gmail.com>
2022-07-25 10:46:45 -0700

    Argc argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,16 +12,14 @@ int main(int argc, char *argv[])
 	int b;
 	int c;
 
-	if (argc == 1)
+	if (argc < 3 || argc > 4)
 	{
 		printf("Error\n");
 		return (1);
-	}
-	
+	}	
 	a = atoi(argv[1]);
         b = atoi(argv[2]);
         c = a * b;
-
 
 	printf("%d\n", c);
 	return (0);
