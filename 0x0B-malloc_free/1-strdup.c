270c9381ea693b51619ed71df460e2ae147f71d9
KaraEno <abasiakaraalexander@gmail.com>
2022-07-27 00:12:23 +0100

    Malloc,free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,7 +14,7 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 
-	for (i = 0; str[i] = '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 		length += 1;
 
 	dup = malloc(sizeof(char) * (length + 1));
