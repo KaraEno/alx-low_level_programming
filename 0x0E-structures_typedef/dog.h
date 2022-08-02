bfa560c7fa2423880f904c37a2afc415dfea583d
KaraEno <abasiakaraalexander@gmail.com>
2022-08-02 08:24:54 -0700

    Structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+
+/**
+*struct dog- just another stray dog
+*@name: name of dog
+*@owner: owner of dog
+*@age: age of the dog
+*/
+
+struct dog {
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
