e4ecc90327c70444386ffba5690543eacb639420
KaraEno <abasiakaraalexander@gmail.com>
2022-08-02 11:07:32 -0700

    Structures typedef

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+*free_dog - initialize the struct
+*@d: dog to be freed
+*/
+void free_dog(dog_t *d)
+{
+	if  (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
