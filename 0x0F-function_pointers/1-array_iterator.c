2e6de19d6c7704eb9f968c9dcdf32353e9f1c409
KaraEno <abasiakaraalexander@gmail.com>
2022-08-03 17:45:40 -0700

    Function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,7 +8,7 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (size == NULL || action == NULL)
+	if (array == NULL || action == NULL)
 		return;
 
 	for (; size > 0; size--)
