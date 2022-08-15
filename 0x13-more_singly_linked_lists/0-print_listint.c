3b0f2bc31c5145aaa18d9ff90accec91d651be84
KaraEno <abasiakaraalexander@gmail.com>
2022-08-15 08:43:33 -0700

    More singly linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "lists.h"
+
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+	while (h != NULL)
+	{
+		i++;
+		printf("%d\n", h->n);
+		h = h->next;
+		return h;	
+	}
+	return i;
+}
